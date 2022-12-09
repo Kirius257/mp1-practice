b77332f25f4a1540fddf409fada6b586c5df2a17
Kirius257 <holin.kirill@yandex.ru>
2022-12-09 21:17:32 +0300

    First goal completed

--- KholinKI/Practice5/Source.c
+++ KholinKI/Practice5/Source.c
@@ -3,31 +3,44 @@
 #include "Sorts.h"
 #include <time.h>
 #include <string.h>
+#include <conio.h>
 
+char path[100]; //path directory
 
 
 int main() {
 	int answer;
-	char path; //path directory
+	char save;
+	char path[100]; //path directory
+	save = path[0];
 	printf("Hello, welcome to the file manager! Choose a method for sorting file structures from the menu: \n");
 	printf("Sorting menu: \n");
 	printf("1. Sorting by choice\n");									//Greeting and sorting selection
 	printf("2. Sorting by inserts\n");
 	printf("3. Hoare Sorting\n");
-	scanf("%d", &answer); //Making choices
+	scanf_s("%d", &answer); //Making choices
+	getchar();
 	
-
-	switch (answer) {
-		case 1:	
-			printf("Enter the path for the directory by which you want to sort the content:\n");
-			scanf("%s", &path); //file path
-			path(path);
-			break;
-		case 2:															
-			break;
-		case 3:
-			break;
+	if (answer == 1) {
+		path[0] = save;
+		printf("Enter the path for the directory by which you want to sort the content:");
+		fgets(path, 100, stdin); //file path
+		puts(path);
 	}
+	else if (answer == 2) {
+		printf("Enter the path for the directory by which you want to sort the content:");
+		fgets(path, 100, stdin); //file path
+		puts(path);
+	}
+	else {
+		printf("Enter the path for the directory by which you want to sort the content:");
+		fgets(path, 100, stdin); //file path
+		puts(path);
+	}
+	/*
+	* Следующая задача:
+	* Придумать,как перейти в директорию
+	*/
 	return 0;
 }
 
